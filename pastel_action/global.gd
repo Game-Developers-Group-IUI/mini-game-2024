fb561b7642f48b7532bd994de69703b9abc0d563
Gordon, Lucas Eugene <lugordon@iu.edu>
2024-09-22 18:01:57 -0400

    godot project

---
+++ pastel_action/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+## List of global signals
+## Emit signal with Global.signalnamehere.emit()
+## Receive signal with Global.signalnamehere.connect(_functionnamehere)
+
+signal activate(value: float)
---
+++ pastel_action/scenes/player.gd
@@ -0,0 +1,55 @@
+class_name Player
+extends CharacterBody2D
+
+## External References
+@onready var game: Game = get_node(^"/root/Game")
+
+## Internal References
+@onready var camera: Camera2D = %Camera2D
+@onready var sprite: Sprite2D = %Sprite2D
+@onready var particles: GPUParticles2D = %GPUParticles2D
+@onready var cooldown_timer: Timer = %CooldownTimer
+
+## Properties
+@export var speed: float = 1000.0 ## Movement speed, pixels per frame
+
+## Variables
+var input_direction: Vector2 = Vector2.ZERO
+var on_cooldown: bool = false
+
+
+func _process(_delta: float) -> void:
+	## Halt all processing if game is paused
+	if game.ui == game.state.paused:
+		return
+	
+	input_direction = Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
+	
+	## Decay speed and set it to zero
+	velocity /= 2
+	if is_zero_approx(velocity.length_squared()):
+		velocity = Vector2.ZERO
+		
+	## Move smoothly
+	if input_direction:
+		velocity = input_direction * speed
+	move_and_slide()
+	
+	## Action
+	if Input.is_action_just_pressed(&"confirm"):
+		activate()
+
+
+func activate() -> void:
+	if not on_cooldown:
+		particles.restart()
+		on_cooldown = true
+		Global.activate.emit(1)
+
+
+func _on_gpu_particles_2d_finished() -> void:
+	cooldown_timer.start()
+
+
+func _on_cooldown_timer_timeout() -> void:
+	on_cooldown = false
