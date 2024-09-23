49d506706c1656b5934bb7de0d384cd5690b5725
Gordon, Lucas Eugene <lugordon@iu.edu>
2024-09-22 22:22:02 -0400

    standardized

--- pastel_action/global.gd
+++ pastel_action/global.gd
@@ -1,7 +1,7 @@
 extends Node
 
 ## List of global signals
-## Emit signal with Global.signalnamehere.emit()
-## Receive signal with Global.signalnamehere.connect(_functionnamehere)
-
-signal activate(value: float)
+## Declare with signal signal_name_here(parameters_here: type_here)
+## Emit signal with Global.signal_name_here.emit(parameters_here)
+## Connect signal in _ready() with Global.signal_name_here.connect(_function_name_here)
+## Receive signal with func _function_name_here(parameters_here: type_here)
--- pastel_action/scenes/level.gd
+++ pastel_action/scenes/level.gd
@@ -1,50 +1,52 @@
+class_name Level
 extends Node2D
 
-class_name Level
+## Internal References
+@onready var ladder_cooldown_timer: Timer = %LadderCooldown
 
-##Internal References
+## Static References
 @export_category("Basement")
-@export var basementFloor: Sprite2D
+@export var basement_floor: Sprite2D
 @export var pentagram: Sprite2D
 
 @export_category("Aboveground")
-@export var abovegroundFloor: Sprite2D
+@export var aboveground_floor: Sprite2D
 
 @export_category("Universal")
 @export var walls: StaticBody2D
 @export var ladder: Area2D
 
 ## Variables
-@export var inBasement := true
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var in_basement: bool = true
+var on_ladder_cooldown: bool = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	##Ladder climb
-	if Input.is_action_just_pressed("confirm"):
-		for body in ladder.get_overlapping_bodies():
-			#print("hi")
-			if body.is_in_group("player"):
-				inBasement = not inBasement
-	
-	
-	
-	
-	##Switch floors
+	## Ladder climbing
+	if Input.is_action_just_pressed(&"confirm"):
+		for body: PhysicsBody2D in ladder.get_overlapping_bodies():
+			if body is Player and not on_ladder_cooldown:
+				on_ladder_cooldown = true
+				ladder_cooldown_timer.start()
+				if in_basement:
+					to_aboveground()
+				elif not in_basement:
+					to_basement()
+
+
+func to_basement() -> void:
 	#TODO: switching active states of interactibles that don't exist yet
-	if inBasement:
-		if abovegroundFloor.visible:
-			abovegroundFloor.hide()
-		if not basementFloor.visible:
-			basementFloor.show()
-	else:
-		if basementFloor.visible:
-			basementFloor.hide()
-		if not abovegroundFloor.visible:
-			abovegroundFloor.show()
-	
-	pass
+	in_basement = true
+	aboveground_floor.hide()
+	basement_floor.show()
+
+
+func to_aboveground() -> void:
+	in_basement = false
+	#TODO: switching active states of interactibles that don't exist yet
+	aboveground_floor.show()
+	basement_floor.hide()
+
+
+func _on_ladder_cooldown_timeout() -> void:
+	on_ladder_cooldown = false
