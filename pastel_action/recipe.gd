0cffe5923b08dad2c339adff05dac087d7b9b9c3
Creepslime <48263288+Creepslime@users.noreply.github.com>
2024-10-04 21:01:18 -0400

    Add recipe resources (nonfunctional)

---
+++ pastel_action/recipe.gd
@@ -0,0 +1,26 @@
+class_name Recipe
+extends Resource
+
+@export var output:int
+@export var slot_1:int
+@export var slot_2:int
+@export var slot_3:int
+@export var slot_4:int
+@export var slot_5:int
+@export var slot_6:int
+@export var slot_7:int
+@export var slot_8:int
+@export var slot_9:int
+@export var slot_10:int
+@export var slot_11:int
+
+var slots:Array[int]
+
+func setup() -> void:
+	## Fill array
+	pass
+
+
+func shuffle_ingredients() -> void:
+	## Shuffle Array
+	pass
