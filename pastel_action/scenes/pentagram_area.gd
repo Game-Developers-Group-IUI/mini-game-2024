2e3f47c14daf2291bcf2700fff83c735a2f4634f
Gordon, Lucas Eugene <lugordon@iu.edu>
2024-10-11 20:42:28 -0400

    fixed ingredients activating pentagram from wrong floor

--- pastel_action/scenes/pentagram_area.gd
+++ pastel_action/scenes/pentagram_area.gd
@@ -1,6 +1,9 @@
 class_name PentagramArea
 extends Area2D
 
+## External References
+@onready var game: Game = get_node(^"/root/Game")
+
 ##Child references
 @export var col : CollisionPolygon2D
 @export var spr : Polygon2D
@@ -16,15 +19,13 @@ func _ready() -> void:
 func check_ingredients() -> Pickup:
 	for ing in get_overlapping_bodies():
 		print("body ", ing.name, ing.collision_layer)
-		if ing is Pickup:
-			if ing.landed:
-				if not ing.PA:
-					if current_ingredient != ing:
-						if current_ingredient is Pickup:
-							current_ingredient.consume()
-						current_ingredient = ing
-						current_ingredient.PA = self
-						pass
+		if ing is Pickup and ing.landed and not ing.penta_area:
+			if ing.get_parent() == game.level.basement_floor:
+				if current_ingredient != ing:
+					if current_ingredient is Pickup:
+						current_ingredient.consume()
+					current_ingredient = ing
+					current_ingredient.penta_area = self
 	
 	if current_ingredient: 
 		if current_ingredient.is_queued_for_deletion():
@@ -32,7 +33,7 @@ func check_ingredients() -> Pickup:
 	
 		if current_ingredient is Pickup:
 			if !current_ingredient.landed:
-				current_ingredient.PA = null
+				current_ingredient.penta_area = null
 				current_ingredient = null
 		
 		if current_ingredient is Pickup:
