2d4a2db74f706ed8ccb89a221ee75b5599d0acd6
Creepslime <48263288+Creepslime@users.noreply.github.com>
2024-10-11 20:37:03 -0400

    recipe work wip

--- pastel_action/recipe.gd
+++ pastel_action/recipe.gd
@@ -18,9 +18,25 @@ var slots:Array[int]
 
 func setup() -> void:
 	## Fill array
+	slots[0] = slot_1
+	slots[1] = slot_2
+	slots[2] = slot_3
+	slots[3] = slot_4
+	slots[4] = slot_5
+	slots[5] = slot_6
+	slots[6] = slot_7
+	slots[7] = slot_8
+	slots[8] = slot_9
+	slots[9] = slot_10
+	slots[10] = slot_11
 	pass
 
 
 func shuffle_ingredients() -> void:
 	## Shuffle Array
+	var slotsRecipeHold := {}
+	var slotsCopy := slots.duplicate()
+	for slot : int in slotsCopy:
+		#if slot in 
+		pass
 	pass
